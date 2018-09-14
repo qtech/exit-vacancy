8dfafa154c5fbc280223194f062a6d2a42036647
shreeraj <shreeraj.qrioustech@gmail.com>
2018-09-14 15:16:52 +0530

    API changes

--- app/Googlemaps.php
+++ app/Googlemaps.php
@@ -8,7 +8,8 @@ class Googlemaps extends Model
 {
     public static function getDistanceandTime($lat1,$long1,$lat2,$long2)
     {
-        $url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=".$lat1.",".$long1."&destinations=".$lat2.",".$long2."&key=AIzaSyAsmt3QtKhPTx2hjAAN2YJnPKjgXBfyfmM";
+        $url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=".$lat1.",".$long1."&destinations=".$lat2.",".$long2."&key=AIzaSyAm06saqas065mdc1zQc8i3SyAvzpdV0XM";
+        
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -20,7 +21,6 @@ class Googlemaps extends Model
         curl_close($ch);
         $response_a = json_decode($response, true);
         //$dist = $response_a['rows'][0]['elements'][0]['distance']['text'];
-        return $response_a;
         $time = $response_a['rows'][0]['elements'][0]['duration']['text'];
 
         return $time;
